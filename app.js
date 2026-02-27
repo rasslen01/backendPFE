2b56720aed17d543df682d528472f7930cad1981
Rasslen Benassi <160419199+rasslen01@users.noreply.github.com>
2026-02-27 17:37:11 +0300

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ require('dotenv').config();
 const { connectDB } = require('./config/db');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/userRouter');
+var osRouter = require('./routes/osRouter');
 
 const req = require('express/lib/request');
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/os' , osRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
