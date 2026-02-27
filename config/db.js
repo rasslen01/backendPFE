0a864672e761081217b4443ea8b5715459658ed8
Rasslen Benassi <160419199+rasslen01@users.noreply.github.com>
2026-02-27 17:37:27 +0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 module.exports.connectDB = async () => {
     mongoose.set('strictQuery', false);
-    mongoose.connect("mongodb+srv://BackendPFEdb:QXaEqr7E8TFyMefg@cluster0.g8k6mmk.mongodb.net/").then(() => {
+    mongoose.connect(process.env.url_DB).then(() => {
         console.log("Connected to MongoDB");
     }).catch((err) => {
         console.log("Error connecting to MongoDB: ", err);
