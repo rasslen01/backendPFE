74ed2c54ae3deef3873260c993de3c19be743f82
Rasslen Benassi <160419199+rasslen01@users.noreply.github.com>
2026-03-02 19:26:34 +0300

    install multer

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use("/uploads", express.static(path.join(__dirname, 'uploads')));
 
 app.use('/users', usersRouter);
 app.use('/os' , osRouter)
